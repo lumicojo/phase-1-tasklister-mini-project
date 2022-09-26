f0a34607bfc6cea912d70aa9fd1b10ee3fd692e5
+ <+>
2022-09-26 16:52:35 -0400

    create new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+function createNewTask(task) {
+  console.log(task);
+  let textNode = document.createTextNode(task);
+  const list = document.getElementById("tasks")
+  list.appendChild(textNode);
+  
+
+}
+
+function formSubmit (event) {
+  event.preventDefault()
+  
+  const formInput = document.getElementById("new-task-description").value
+  createNewTask(formInput)
+}
+
+const taskForm = document.getElementById("create-task-form");
+taskForm.addEventListener("submit", (event) => 
+
+
+
+
+
